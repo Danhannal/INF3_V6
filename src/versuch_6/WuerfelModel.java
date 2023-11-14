f3ac64ba50c746b11adb592c1243aa3ed2ec0411
Kieran <kieranh@web.de>
2023-11-14 11:48:39 +0000

    added functions to basic model

--- src/versuch_6/WuerfelModel.java
+++ src/versuch_6/WuerfelModel.java
@@ -5,24 +5,47 @@
 
 package versuch_6;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.SubmissionPublisher;
+
 /**
  *
  * @author Kieran
  */
-public class WuerfelModel implements Runnable 
+public class WuerfelModel// implements Runnable 
 {
   private int runFrom;
   private int runTo;
   private int currentNumber;
   private boolean running;
+  private ExecutorService eService;
+  private SubmissionPublisher <Integer> numberPublisher;
+  private SubmissionPublisher <Boolean> statusPublisher;
   public WuerfelModel()
   {
     runFrom = 1;
     runTo = 6;
     running = false;
+    eService = Executors.newSingleThreadExecutor();
+    numberPublisher = new SubmissionPublisher<>();
+    statusPublisher = new SubmissionPublisher<>();
+  }
+  public void addValueSubscriber(Subscriber<Integer> subscriber)
+  {
+    numberPublisher.subscribe(subscriber);
+  }
+  public void addStatusSubscriber(Subscriber<Boolean> subscriber)
+  {
+    statusPublisher.subscribe(subscriber);
   }
 
-  @Override
+  public void start()
+  {
+    
+  }
+  //@Override
   public void run()
   {
     while(running)
