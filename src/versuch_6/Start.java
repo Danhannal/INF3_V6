f49800898c041c7c3b05ccb1be3cc9fdfdb67156
Daniel Bleich <dbleich@outlook.de>
2023-11-15 15:01:51 +0100

    Added adapter initialization to Start class

--- src/versuch_6/Start.java
+++ src/versuch_6/Start.java
@@ -4,6 +4,7 @@
  */
 package versuch_6;
 
+import adapter.Adapter;
 import controller.CommandController;
 import view.*;
 
@@ -21,7 +22,8 @@ public class Start {
     {
         var view = new MainWindow();
         var model = new WuerfelModel();
-        var controller = new CommandController(view, model);
+        var observer = new Adapter(view, model);
+        var controller = new CommandController(view, model, observer);
         controller.registerCommands();
         controller.registerEvents();
         view.setVisible(true);
