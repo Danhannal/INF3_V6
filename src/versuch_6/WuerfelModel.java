25f6ea4b4203ab002e1e120fd177d638377e0a38
Kieran <kieranh@web.de>
2023-11-17 11:51:04 +0000

    added runnable thread for the model

--- src/versuch_6/WuerfelModel.java
+++ src/versuch_6/WuerfelModel.java
@@ -5,11 +5,6 @@
 
 package versuch_6;
 
-import java.lang.System.Logger;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Flow.Subscriber;
-import java.util.concurrent.SubmissionPublisher;
 
 /**
  *
@@ -17,36 +12,17 @@ import java.util.concurrent.SubmissionPublisher;
  */
 public class WuerfelModel// implements Runnable 
 {
-  private int runFrom;
-  private int runTo;
-  private int currentNumber;
-  private boolean running;
-  private ExecutorService eService;
-  private SubmissionPublisher <Integer> numberPublisher;
-  private SubmissionPublisher <Boolean> statusPublisher;
+  private WuerfelThread runningThread;
   public WuerfelModel()
   {
-    runFrom = 1;
-    runTo = 6;
-    running = false;
-    eService = Executors.newSingleThreadExecutor();
-    numberPublisher = new SubmissionPublisher<>();
-    statusPublisher = new SubmissionPublisher<>();
-  }
-  public void addValueSubscriber(Subscriber<Integer> subscriber)
-  {
-    numberPublisher.subscribe(subscriber);
-  }
-  public void addStatusSubscriber(Subscriber<Boolean> subscriber)
-  {
-    statusPublisher.subscribe(subscriber);
+    runningThread = new WuerfelThread();
   }
 
   public void start()
   {
     //V1 start publishing thread
-    statusPublisher.submit(true);
-    running = true;
+    runningThread.statusPublisher.submit(true);
+    runningThread.running = true;
     //V2 create thread to run in
     
     //V3 notify to exit wait state of thread
@@ -54,32 +30,9 @@ public class WuerfelModel// implements Runnable
     public void stop()
   {
     //V1 Stop publishing
-    statusPublisher.submit(false);
-    running = false;
+    runningThread.statusPublisher.submit(false);
+    runningThread.running = false;
     //V2 destroy thread
     //V3 wait thread
-  }
-  //@Override
-  public void run()
-  {
-    while(true)
-    {
-      try
-      {
-        Thread.sleep(20);
-      }
-      catch(InterruptedException exception)
-      {
-        //Logger.getLogger(WuerfelModel.class.getName().severe(exception.toString()));
-      }
-      if(currentNumber >= runTo){currentNumber = runFrom;}
-      else{currentNumber +=1;}
-      //publish new number
-      if (running ==true)
-      {
-      numberPublisher.submit(currentNumber);
-      //für status das code läuft möglicherweise überflüssig
-      }
-    }
   }
 }
---
+++ src/versuch_6/WuerfelThread.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package versuch_6;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
+
+/**
+ *
+ * @author Kieran
+ */
+public class WuerfelThread implements Runnable
+{
+  private int runFrom;
+  private int runTo;
+  private int currentNumber;
+  public boolean running;
+  private ExecutorService eService;
+  private SubmissionPublisher <Integer> numberPublisher;
+  public SubmissionPublisher <Boolean> statusPublisher;
+  
+  
+  public WuerfelThread()
+  {
+    runFrom = 1;
+    runTo = 6;
+    running = false;
+    eService = Executors.newSingleThreadExecutor();
+    numberPublisher = new SubmissionPublisher<>();
+    statusPublisher = new SubmissionPublisher<>();
+  
+  }
+  public void addValueSubscriber(Flow.Subscriber<Integer> subscriber)
+  {
+    numberPublisher.subscribe(subscriber);
+  }
+  public void addStatusSubscriber(Flow.Subscriber<Boolean> subscriber)
+  {
+    statusPublisher.subscribe(subscriber);
+  }
+
+
+  @Override
+  public void run()
+  {
+    while(true)
+    {
+      try
+      {
+        Thread.sleep(20);
+      }
+      catch(InterruptedException exception)
+      {
+        //Logger.getLogger(WuerfelModel.class.getName().severe(exception.toString()));
+      }
+      if(currentNumber >= runTo){currentNumber = runFrom;}
+      else{currentNumber +=1;}
+      //publish new number
+      if (running ==true)
+      {
+      numberPublisher.submit(currentNumber);
+      //für status das code läuft möglicherweise überflüssig
+      }
+    }
+  }
+}
